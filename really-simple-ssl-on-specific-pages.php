298ac84362d68f2d726527e0050029b9cd147a5f
Rogier Lankhorst <rogierlankhorst@gmail.com>
2018-03-19 20:01:54 +0100

    brute force merge

--- really-simple-ssl-on-specific-pages.php
+++ really-simple-ssl-on-specific-pages.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple SSL on specific pages
  * Plugin URI: https://www.really-simple-ssl.com
  * Description: Lightweight plugin without any setup to make your site ssl proof
- * Version: 2.0.1
+ * Version: 2.0.2
 
  * Text Domain: really-simple-ssl-specific-pages
  * Domain Path: /languages
