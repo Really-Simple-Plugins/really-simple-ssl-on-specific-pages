76323cc9ea1ba9fdfba5b0378ac65ade594ef397
mwolters90 <mark@really-simple-ssl.com>
2019-06-18 09:48:48 +0200

    version readme

--- really-simple-ssl-on-specific-pages.php
+++ really-simple-ssl-on-specific-pages.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple SSL on specific pages
  * Plugin URI: https://www.really-simple-ssl.com
  * Description: Lightweight plugin without any setup to make your site ssl proof
- * Version: 2.0.12
+ * Version: 2.0.13
  * Text Domain: really-simple-ssl-specific-pages
  * Domain Path: /languages
  * Author: Rogier Lankhorst
